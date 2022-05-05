7ea860fb228dcc3a631eeb4e07f4085020e650d2
developer <nitsisodiya@bestpeers.com>
2022-05-05 16:00:19 +0530

    first commit

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+require("./connection");
+
+const dotenv = require("dotenv");
+dotenv.config();
+const port = process.env.PORT || 8000;
+
+const routers = require("./routers");
+var cors = require("cors");
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cors());
+app.use(routers);
+
+app.listen(port, () => {
+  console.log(`Server is crreated ${port}`);
+});
